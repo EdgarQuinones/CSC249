b88c727b8c0c4ed036e8c4997003a740ca3f2688
EJ <50989656+UnEvolvedGod@users.noreply.github.com>
2023-02-27 16:36:40 -0500

    Create Playlist.java

---
+++ Playlist.java
@@ -0,0 +1,23 @@
+package project1;
+
+public class Playlist {
+	
+	//private instance variables
+	private Song head = new Song();
+	private Song tail = new Song();
+	private int size;
+	
+	//no-arg constructor
+	
+	//constructor
+	
+	//method accepts 1 song and adds to end of playlist
+	
+	//method accepts 2 songs and adds 2nd song after 1st
+	
+	//method accepts 1 song and removes that song from playlist
+	
+	//toString method prints all songs, number of songs in 
+	//the playlist, and length of playlist in minutes and seconds
+	
+}
