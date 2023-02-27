cedbdb1459eb9e40c3dd6640070f5722174c0cb8
EJ <50989656+UnEvolvedGod@users.noreply.github.com>
2023-02-27 16:36:01 -0500

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,57 @@
+package project1;
+
+/* 
+ * Name: Edgar Quinones
+ * Date: 2/27/2023
+ * 
+ * Purpose of Program:
+ * 
+ * Use the following information to create a program that allows the user 
+ * to manipulate a music playlist and view its content. Your program should 
+ * contain 3 separate classes: a Song class, a Playlist class, and a Demo 
+ * class. In this program, the Playlist class will function as a doubly linked
+ * list that contains Song objects. The Demo class will be used to make 
+ * modifications to the Playlist. 
+ */
+public class Demo {
+
+	public static void main(String[] args) {
+
+		// Playlist object
+
+		/*
+		 * display menu allow user following options:
+		 * 
+		 * 1. add dont to playlist
+		 * 
+		 * 2. insert new song after the song that is currently playing (If playlist
+		 * empty, it should become currently playing song)
+		 * 
+		 * 3. print contents of playlist
+		 * 
+		 * 4. display current song (When displaying the current song for the first time,
+		 * the first song in the playlist should be displayed. To accomplish this,
+		 * create a Song reference variable and assign it the first Song in the
+		 * Playlist)
+		 * 
+		 * 5. remove current song(new current song will be song after)
+		 * 
+		 * 6. skip to the next song(if end of playlist, go back to first song
+		 * 
+		 * 7. return to previous song (If you are at the beginning of the Playlist, the
+		 * previous Song should be the last Song in the playlist.)
+		 * 
+		 * Require valid input from user, if input wrong, correct user and ask again
+		 * 
+		 * If any of the options chosen would not work due to there being no applicable
+		 * songs in the playlist, an appropriate error message should be displayed.
+		 * 
+		 * The user should be allowed to repeatedly select menu options until they
+		 * choose to Exit.
+		 */
+
+	}
+
+}
+
+
